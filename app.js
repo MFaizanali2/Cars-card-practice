88700a32eb2b04ca76c5109857463d4d011d4565
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-22 16:38:08 +0500

    js line 92
    continue

---
+++ app.js
@@ -0,0 +1,93 @@
+let cars = [
+    {
+      "brand": "Toyota",
+      "famousModel": "Camry",
+      "color": "Super White",
+      "year": 1982
+    },
+    {
+      "brand": "Ford",
+      "famousModel": "Mustang",
+      "color": "Race Red",
+      "year": 1964
+    },
+    {
+      "brand": "BMW",
+      "famousModel": "3 Series",
+      "color": "Alpine White",
+      "year": 1975
+    },
+    {
+      "brand": "Mercedes-Benz",
+      "famousModel": "C-Class",
+      "color": "Obsidian Black",
+      "year": 1993
+    },
+    {
+      "brand": "Honda",
+      "famousModel": "Civic",
+      "color": "Crystal Black Pearl",
+      "year": 1972
+    },
+    {
+      "brand": "Tesla",
+      "famousModel": "Model S",
+      "color": "Midnight Silver Metallic",
+      "year": 2012
+    },
+    {
+      "brand": "Audi",
+      "famousModel": "A4",
+      "color": "Ibis White",
+      "year": 1994
+    },
+    {
+      "brand": "Chevrolet",
+      "famousModel": "Corvette",
+      "color": "Torch Red",
+      "year": 1953
+    },
+    {
+      "brand": "Nissan",
+      "famousModel": "Altima",
+      "color": "Gun Metallic",
+      "year": 1992
+    },
+    {
+      "brand": "Hyundai",
+      "famousModel": "Elantra",
+      "color": "Phantom Black",
+      "year": 1990
+    },
+    {
+      "brand": "Volkswagen",
+      "famousModel": "Golf",
+      "color": "Pure White",
+      "year": 1974
+    },
+    {
+      "brand": "Porsche",
+      "famousModel": "911",
+      "color": "Guards Red",
+      "year": 1964
+    },
+    {
+      "brand": "Lexus",
+      "famousModel": "RX",
+      "color": "Nebula Gray Pearl",
+      "year": 1998
+    },
+    {
+      "brand": "Subaru",
+      "famousModel": "Outback",
+      "color": "Crystal White Pearl",
+      "year": 1994
+    },
+    {
+      "brand": "Ferrari",
+      "famousModel": "488",
+      "color": "Rosso Corsa",
+      "year": 2015
+    }
+  ]
+  
